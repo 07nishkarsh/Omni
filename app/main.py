bd9eb14ca211a15fee84c8d05cf81a266405c6d4
07nishkarsh <07kr.nishkarsh@gmail.com>
2026-07-13 17:08:38 +0530

    Fix ASGI app import for Render deploy

---
+++ app/main.py
@@ -0,0 +1,4 @@
+from app import app, create_app
+
+# This file exists solely to satisfy the default Render/Uvicorn start command
+# if it is accidentally set to 'uvicorn app.main:create_app' or 'uvicorn app.main:app'
